a93ad686499557cd00a26e09f5738d4679ad6f01
nichapat_th <inky_pegasus@hotmail.com>
2023-09-04 18:26:57 +0700

    finish

--- components/donation.tsx
+++ components/donation.tsx
@@ -1,9 +1,32 @@
 // import { API_URL } from "../utils/api";
 // import { type Donation } from "@/utils/types";
+import { useState, useEffect } from "react";
 import { Paper, Text, Stack, Group, Title, Card } from "@mantine/core";
 import dayjs from "dayjs";
 
+type DonationType = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  amount: number;
+  time: string;
+};
+
 export default function Donation() {
+  const [donations, setDonations] = useState<DonationType[]>([]);
+
+  useEffect(() => {
+    fetch("https://donation-server-production.up.railway.app/donation")
+      .then((response) => response.json())
+      .then((data) => {
+        setDonations(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <Card withBorder shadow="xs" bg="gray.3">
       <Group mb={20}>
@@ -18,33 +41,17 @@ export default function Donation() {
         </Title>
       </Group>
       <Stack>
-        <Paper shadow="xs" p="md">
-          <Group>
-            <Text>Tom</Text>
-            <Text>Sawyer</Text>
-            <Text>tom_sawyer@gmail.com</Text>
-            <Text>10000</Text>
-            <Text>{dayjs("2023-08-26 06:17:51").format("D-MMM HH:mm:ss")}</Text>
-          </Group>
-        </Paper>
-        <Paper shadow="xs" p="md">
-          <Group>
-            <Text>Tom</Text>
-            <Text>Sawyer</Text>
-            <Text>tom_sawyer@gmail.com</Text>
-            <Text>10000</Text>
-            <Text>{dayjs("2023-08-26 06:17:51").format("D-MMM HH:mm:ss")}</Text>
-          </Group>
-        </Paper>
-        <Paper shadow="xs" p="md">
-          <Group>
-            <Text>Tom</Text>
-            <Text>Sawyer</Text>
-            <Text>tom_sawyer@gmail.com</Text>
-            <Text>10000</Text>
-            <Text>{dayjs("2023-08-26 06:17:51").format("D-MMM HH:mm:ss")}</Text>
-          </Group>
-        </Paper>
+        {donations.map((donation) => (
+          <Paper key={donation.id} shadow="xs" p="md">
+            <Group>
+              <Text>{donation.firstName}</Text>
+              <Text>{donation.lastName}</Text>
+              <Text>{donation.email}</Text>
+              <Text>{donation.amount}</Text>
+              <Text>{dayjs(donation.time).format("D-MMM HH:mm:ss")}</Text>
+            </Group>
+          </Paper>
+        ))}
       </Stack>
     </Card>
   );
--- components/form.tsx
+++ components/form.tsx
@@ -1,7 +1,57 @@
 // import { API_URL } from "@/utils/api";
+import { useState } from "react";
 import { Input, Button, Card, Title, Stack } from "@mantine/core";
 
 export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const donationData = {
+      firstName,
+      lastName,
+      email,
+      amount,
+    };
+    try {
+      const response = await fetch(
+        "https://donation-server-production.up.railway.app/donate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(donationData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to submit donation.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    fetch("https://donation-server-production.up.railway.app/donate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // ... your form data ...
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // ... handle the response, maybe show a success message ...
+      })
+      .catch((error) => {
+        console.error("Error submitting data:", error);
+      });
+  };
   return (
     <Card withBorder shadow="xs" p="xl" bg="cyan.2">
       <Title order={1} color="blue">
